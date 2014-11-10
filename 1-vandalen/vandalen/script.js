10da5d6818c817bcf7a3c3c07cdfdbd694da594d
kn222gp <kn222gp@student.lnu.se>
2014-11-10 20:02:55 +0000

    needs some fine adjustment

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,8 +2,52 @@
 
 var makePerson = function(persArr){
 
+/*-----------------------------------Variabler-------------------------------*/
+	
+	var names ="";
+	var minAge;
+	var maxAge;
+	var averageAge;
+	var nameArray = [];
+	var ageArray = [];
+	var result = {};
 
-	// Din kod här...
 
-}
+/*----------------------------------Namn-------------------------------------*/
 
+	for (var i = 0; i<persArr.length; i++){
+	    nameArray[i] = persArr[i].name;
+	}
+	
+	nameArray.sort(function (a,b) { return a.localeCompare(b, 'sv'); });
+	names = nameArray.toString();
+	names = names.split(",").join(", ");
+
+
+/*----------------------------------Ålder------------------------------------*/
+	
+	for (var i = 0; i<persArr.length; i++){
+	    ageArray[i] = persArr[i].age;
+	}
+	
+	minAge = Math.min().apply(Math, ageArray);
+	maxAge = Math.max().apply(Math, ageArray);
+	averageAge = ageArray.reduce(function(a, b) { return a + b });
+	averageAge = averageAge / ageArray.length;
+
+
+/*----------------------------Slut på makePerson ----------------------------*/	
+	result = {minAge: minAge, maxAge: maxAge, averageAge: Math.Round(averageAge), names: names};
+	return result;
+	
+};
+
+/*------------------ Resterande kod utanför makePerson-----------------------*/	
+
+
+	var data = [{name: "John Häggerud", age: 37}, 
+	            {name: "Johan Leitet", age: 36}, 
+	            {name: "Mats Loock", age: 46}];
+	var result = makePerson(data);
+
+    console.log(result);
