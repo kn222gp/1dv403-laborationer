f845db1d2b508bf0750222a244fce7963ac6fc03
kn222gp <kn222gp@student.lnu.se>
2014-12-09 05:32:50 +0000

    A few things left.

--- 4-thequiz/myScript.js
+++ 4-thequiz/myScript.js
@@ -3,79 +3,91 @@
 var quiz = {
     
     // Declare variables.
-    ask: document.getElementById("firstSec"),
+    msg : document.getElementById("msg"),
     qSec: document.getElementById("question"),
-    mess: document.getElementById("sndSection"),
+    error: document.getElementById("error"),
+    
     answer: document.getElementById("text"),
     submit: document.getElementById("submit"),
+    
     tries: 0,
+    tryArray: [],
     xhr: new XMLHttpRequest(),
     q: null,
-    adress: "http://vhost3.lnu.se:20080/question/1",
            
     init: function(){      
         console.log("window loaded."),
         
-        // When I click the button.
-        quiz.submit.addEventListener("click", function(e){
-           console.log("clicked."),
-           e.preventDefault();
-           quiz.asking(quiz.q.nextURL, quiz.answer.value);
-        });
-        
-        quiz.quizGame();
+        quiz.quizGame("http://vhost3.lnu.se:20080/question/1");
     },
     
     // The function that contacts the server.
-    quizGame: function(){
+    quizGame: function(url){
         
         quiz.xhr.onreadystatechange = function(){
             
-             if(quiz.xhr.readyState === 4 && quiz.xhr.status === 200){
+            if(quiz.xhr.readyState === 4 && quiz.xhr.status === 200){
                  quiz.q = JSON.parse(quiz.xhr.responseText);
-                 var show = quiz.q.question;
-                 quiz.qSec.innerHTML = show;
+                 quiz.qSec.innerHTML = quiz.q.question;
+             
+            // When I click the button.
+            quiz.submit.addEventListener("click", function(e){
+               console.log("clicked."),
+               e.preventDefault();
+               quiz.asking(quiz.q.nextURL, quiz.answer.value);
+            });     
+            
              }
         };
         
-        quiz.xhr.open("GET", quiz.adress, true);
+        quiz.xhr.open("GET", url, true);
         quiz.xhr.send(null);
     },
 
     asking: function(url, ans){
     
+    
+    
     var xhr2 = new XMLHttpRequest();
+    console.log("test");
     
         xhr2.onreadystatechange = function(){
+            
             if(xhr2.readyState === 4){
                 
                 var message = JSON.parse(xhr2.responseText);
-                console.log(message);
                 
                 if(message.message === "Correct answer!"){
                     
-                    if(message.nextURL !== undefined)
-                    {
+                    if(message.nextURL !== undefined){
+                        quiz.tryArray.push(quiz.tries);
                         quiz.quizGame(message.nextURL);
+                        quiz.msg.innerHTML = "Rätt!";
                     }
+                    else{
+                       
+                        
+                    }
+                    
                 }
-                
-                else{
-                    quiz.ask.innerHTML = "Fel svar!";    
+                else {
+                    quiz.tries++;
+                    console.log("NEJ");
+                    quiz.error.innerHTML = "Fel svar, tyvärr!"; 
+                  
                 }
-                
             }
         };
     
     
     var json = { answer: ans };
-    var jsonSring = JSON.stringify(json);
+    var jsonString = JSON.stringify(json);
         
         quiz.xhr.open("POST", url, true);
-        quiz.xhr.setRequestHeader('Content-Type', 'application/json');
-        quiz.xhr.send(jsonSring);
+        quiz.xhr.setRequestHeader("Content-Type", "application/json");
+        quiz.xhr.send(jsonString);
         
-    }
+    },
     
 }; 
 window.onload = quiz.init;
